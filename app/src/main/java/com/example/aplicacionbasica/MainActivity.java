ab802efb0f230d5ee83d6867d42549c07c4980cd
Sergio Medina <hola@sergiomedina.io>
2023-10-09 10:27:25 -0300

    feat: add dynamic image on button click

--- app/src/main/java/com/example/aplicacionbasica/MainActivity.java
+++ app/src/main/java/com/example/aplicacionbasica/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -19,10 +20,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View button) {
                 LinearLayout content = findViewById(R.id.content);
+                ImageView newImage = new ImageView(button.getContext());
+                newImage.setBackgroundResource(R.drawable.ic_launcher_foreground);
                 TextView newText = new TextView(button.getContext());
                 newText.setText(R.string.textItem);
                 newText.setBackgroundColor(+R.color.purple_200);
                 content.addView(newText);
+                content.addView(newImage);
             }
         });
     }
